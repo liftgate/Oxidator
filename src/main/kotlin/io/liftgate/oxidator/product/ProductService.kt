522ff9f48f1e01d3f93689c5d5cba46cd6ab89ec
GrowlyX <growlyxdev@gmail.com>
2024-06-28 22:13:20 -0400

    feat: license and products

---
+++ src/main/kotlin/io/liftgate/oxidator/product/ProductService.kt
@@ -0,0 +1,9 @@
+package io.liftgate.oxidator.product
+
+/**
+ * @author GrowlyX
+ * @since 6/28/2024
+ */
+class ProductService
+{
+}
--- src/main/kotlin/io/liftgate/oxidator/product/license/License.kt
+++ src/main/kotlin/io/liftgate/oxidator/product/license/License.kt
@@ -1,16 +1,17 @@
-package io.liftgate.oxidator.product
+package io.liftgate.oxidator.product.license
 
 import io.liftgate.oxidator.utilities.snowflake
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 /**
  * @author GrowlyX
  * @since 6/28/2024
  */
-@Document(collection = "products")
-data class Product(
+@Document(collection = "licenses")
+data class License(
     @Id val id: Long = snowflake(),
-    val name: String,
-    val description: String
+    val discordUser: Long,
+    @Indexed val associatedTxnID: String
 )
